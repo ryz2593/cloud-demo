8775acbadfa8dee282ad3fc4f807b8fc3db77b23
ryz2593 <805445415@qq.com>
2020-01-15 10:13:17 +0800

    consumer注册到eureka,并根据服务id动态获取服务实例，进行服务调用

--- consumer-demo/src/main/java/cn/itcast/ConsumerApplication.java
+++ consumer-demo/src/main/java/cn/itcast/ConsumerApplication.java
@@ -2,6 +2,7 @@ package cn.itcast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
  * @author ryz2593
  * @date 2020/1/14 17:18
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerApplication {
     @Bean
