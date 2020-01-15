f4a6a90b035bf77cdc70715bc0564cb48e829704
ryz2593 <805445415@qq.com>
2020-01-15 10:11:52 +0800

    user-service增加eureka

--- user-service/src/main/java/cn/itcast/UserApplication.java
+++ user-service/src/main/java/cn/itcast/UserApplication.java
@@ -2,12 +2,14 @@ package cn.itcast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author ryz2593
  * @date 2020/1/14 17:04
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("cn.itcast.user.mapper")
 public class UserApplication {
