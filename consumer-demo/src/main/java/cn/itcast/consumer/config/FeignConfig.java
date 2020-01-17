5cef514a1f0b31fa9ac60b222d3392626de6a575
ryz2593 <805445415@qq.com>
2020-01-17 16:37:27 +0800

    feign的日志级别设置

--- consumer-demo/src/main/java/cn/itcast/consumer/config/FeignConfig.java
+++ consumer-demo/src/main/java/cn/itcast/consumer/config/FeignConfig.java
@@ -1,8 +1,18 @@
 package cn.itcast.consumer.config;
 
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
+ * feign 的日志级别设置
  * @author ryz2593
  * @date 2020/1/17 16:07
  */
+@Configuration
 public class FeignConfig {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 }
